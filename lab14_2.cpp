8435073a25e14a135f2c2b70a6e508afa12dd879
Raiwin Inthasit <andaraiwin@gmail.com>
2021-12-28 19:16:43 +0700

    lab14_2 Completed

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,34 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool image[N][M]){
+    cout << "------------------------------------------------------------------------";
+    for(int i = 0; i < N; i++){
+        cout << "\n" << '|';
+        for(int j = 0 ;j < M; j++){
+            if(image[i][j] == 0){
+            cout << " ";
+            }else if(image[i][j] == 1){
+                cout << "*";
+            }
+        }
+        cout << '|';
+    }
+
+    cout << "\n------------------------------------------------------------------------\n";
+    return;
+}
+
+void updateImage(bool image[][M],int s,int x,int y){
+    double C;
+    for(int k = 0; k < N; k++){
+        for(int l = 0; l < M; l++){
+            C = sqrt(pow((k - x), 2) + pow((l - y), 2)) + 1;
+            if(C <= s){
+                image[k][l] = 1;
+            }
+        }
+    }
+    return;
+}
